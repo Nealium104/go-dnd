cbd922b14bdc6bf27654453dc92f032fbf00b176
Nealium104 <neal.grindstaff@gmail.com>
2024-08-18 16:25:05 -0400

    Add json functionality

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand/v2"
+	"os"
+	"sort"
 )
 
 type StatsList struct {
@@ -22,16 +26,81 @@ type StatsList struct {
 
 type Character struct {
 	StatsList
-	Name           string
-	BaseHealth     int
-	InitativeBonus int
-	Initative      int
-	ItemList       []Item
+	Name           string `json:"name"`
+	BaseHealth     int    `json:"BaseHealth"`
+	InitativeBonus int    `json:"InitativeBonus"`
+	Initative      int    `json:"Initative"`
+	ItemList       []Item `json:"ItemList"`
 }
 
 type Item struct {
 	Name  string
 	Bonus int
+}
+
+type CombatList struct {
+	CharacterList []Character
+}
+
+func roll() int {
+	return rand.IntN(20)
+}
+
+func rollInitative(character Character) {
+	roll := roll()
+	character.Initative = roll + character.InitativeBonus
+	fmt.Printf("%v rolled a %v. Their bonus is %v, so their initative is %v\n", character.Name, roll, character.InitativeBonus, character.Initative)
+}
+
+func sortOrderByInitative(characterSlice []Character) []Character {
+	sort.Slice(characterSlice, func(i, j int) bool {
+		return characterSlice[i].Initative > characterSlice[j].Initative
+	})
+	return characterSlice
+}
+
+func deriveBonus(stat int) int {
+	bonus := (stat - 10) / 2
+	return bonus
+}
+
+func createCharacter(name string, health int, initativeBonus int) Character {
+	c := Character{Name: name, BaseHealth: health, InitativeBonus: initativeBonus}
+	saveCharacter(c)
+	return c
+}
+
+func promptCharacter() Character {
+	var name string
+	var health int
+	var initativeBonus int
+	fmt.Println("What is the name of this character?")
+	fmt.Scanln(&name)
+	fmt.Printf("What is the health of %v?\n", name)
+	fmt.Scanln(&health)
+	fmt.Printf("What is the initative bonus of %v?\n", name)
+	fmt.Scanln(&initativeBonus)
+	saveCharacter(createCharacter(name, health, initativeBonus))
+	return createCharacter(name, health, initativeBonus)
+}
+
+func saveCharacter(c Character) {
+	marshaled, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println("Marshal err:", err)
+		return
+	}
+
+	file, err := os.Create(fmt.Sprintf("%v.json", c.Name))
+	if err != nil {
+		log.Fatalf("error with file creation: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(marshaled)
+	if err != nil {
+		log.Fatalf("Error writing to file: %v", err)
+	}
 }
 
 func main() {
@@ -40,7 +109,7 @@ func main() {
 	fmt.Println("Hello, go-dnd! How many monsters and characters will you be using?")
 	fmt.Scanln(&amount)
 	for i := 0; i < amount; i++ {
-		characterSlice = append(characterSlice, createCharacter())
+		characterSlice = append(characterSlice, promptCharacter())
 	}
 	fmt.Println("Rolling initative!")
 	for i := 0; i < len(characterSlice); i++ {
@@ -49,7 +118,7 @@ func main() {
 	}
 	sorted := sortOrderByInitative(characterSlice)
 	for i := 0; i < len(sorted); i++ {
-		fmt.Printf("Character name is %v, and their health is at %v\n", sorted[i].Name, sorted[i].Health)
+		fmt.Printf("Character name is %v, and their health is at %v\n", sorted[i].Name, sorted[i].BaseHealth)
 	}
 	for i := 0; i < len(sorted); i++ {
 		character := sorted[i]
@@ -64,51 +133,4 @@ func main() {
 			i = -1
 		}
 	}
-}
-
-func roll() int {
-	return rand.IntN(20)
-}
-
-func rollInitative(character Character) {
-	roll := roll()
-	character.Initative = roll + character.InitativeBonus
-	fmt.Printf("%v rolled a %v. Their bonus is %v, so their initative is %v\n", character.Name, roll, character.InitativeBonus, roll+character.InitativeBonus)
-}
-
-func sortOrderByInitative(characterSlice []Character) []Character {
-	if len(characterSlice) < 2 {
-		return characterSlice
-	} else {
-		pivot := characterSlice[0]
-		less := []Character{}
-		greater := []Character{}
-
-		for _, i := range characterSlice[1:] {
-			if i.Initative >= pivot.Initative {
-				less = append(less, i)
-			} else {
-				greater = append(greater, i)
-			}
-		}
-
-		sortedLess := sortOrderByInitative(less)
-		sortedGreater := sortOrderByInitative(greater)
-
-		return append(append(sortedLess, pivot), sortedGreater...)
-	}
-
-}
-
-func createCharacter() Character {
-	var name string
-	var health int
-	var initativeBonus int
-	fmt.Println("What is the name of this character?")
-	fmt.Scanln(&name)
-	fmt.Printf("What is the health of %v?\n", name)
-	fmt.Scanln(&health)
-	fmt.Printf("What is the initative bonus of %v?\n", name)
-	fmt.Scanln(&initativeBonus)
-	return Character{Name: name, Health: health, InitativeBonus: initativeBonus}
 }
--- main_test.go
+++ main_test.go
@@ -12,26 +12,35 @@ import (
 // 	ItemList       []Item
 // }
 
-func TestSortOrderByInitative(t *testing.T) {
-	cSlice := []Character{}
-	result := sortOrderByInitative(cSlice)
-	if result != []Character{
-		{
-			Name:           "Billy",
-			Health:         10,
-			InitativeBonus: 3,
-		},
-		{
-			Name:           "Bob",
-			Health:         20,
-			InitativeBonus: 2,
-		},
-		{
-			Name:           "Jones",
-			Health:         30,
-			InitativeBonus: 1,
-		},
-	} {
-		t.Errorf("Result was incorrect %v", 8)
+// func TestSortOrderByInitative(t *testing.T) {
+// 	cSlice := []Character{}
+// 	result := sortOrderByInitative(cSlice)
+// 	if result != []Character{
+// 		{
+// 			Name:           "Billy",
+// 			BaseHealth:     10,
+// 			InitativeBonus: 3,
+// 		},
+// 		{
+// 			Name:           "Bob",
+// 			BaseHealth:     20,
+// 			InitativeBonus: 2,
+// 		},
+// 		{
+// 			Name:           "Jones",
+// 			BaseHealth:     30,
+// 			InitativeBonus: 1,
+// 		},
+// 	} {
+// 		t.Errorf("Result was incorrect %v", 8)
+// 	}
+// }
+
+func TestDeriveBonus(t *testing.T) {
+	result := deriveBonus(20)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("deriveBonus(20) = %d; want %d", result, expected)
 	}
 }
