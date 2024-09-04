05c92cd503e8bb67f674aec0a3177cd686cd0e8b
Neal Powers <neal.grindstaff@gmail.com>
2024-09-04 16:38:21 -0400

    add first iteration of stats list

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -17,22 +18,57 @@ type Character struct {
 	Name   string `json:"name"`
 	Id     string `json:"id"`
 	Health int    `json:"health"`
+	Str    int    `json:"str"`
+	Dex    int    `json:"dex"`
+	Con    int    `json:"con"`
+	Int    int    `json:"int"`
+	Wis    int    `json:"wis"`
+	Cha    int    `json:"cha"`
+	Stats
+}
+
+type Stats struct {
+	Strength     int
+	Dexterity    int
+	Constitution int
+	Intelligence int
+	Wisdom       int
+	Charisma     int
 }
 
 func main() {
 	characterDirectory = "./assets/characters/"
-	c := gatherCLICharacter()
-
-	c.writeCharacter(characterDirectory)
-	displayCharacter(c)
-
-	allCharacters := allCharacters()
-	for _, c := range allCharacters {
-		fmt.Println(c.Name)
+	for {
+		cliOptions()
 	}
+}
 
-	character := loadCharacter(c.Id)
-	fmt.Printf("loadCharacter Function Output: %v\n", character.Name)
+// func calculateStatBonus(stat int) int {
+// 	statBonus := (stat - 10) / 2
+// 	return statBonus
+// }
+
+func cliOptions() {
+	blue := "\033[34m"
+	reset := "\033[0m"
+	var command string
+
+	fmt.Println(string(blue), "What would you like to do?", string(reset))
+	fmt.Println("c to create a character, l for list all current characters, or e to exit")
+	fmt.Scanln(&command)
+
+	if strings.ToLower(command) == "c" {
+		c := gatherCLICharacter()
+		c.writeCharacter(characterDirectory)
+		displayCharacter(c)
+	} else if strings.ToLower(command) == "l" {
+		allCharacters := allCharacters()
+		for _, c := range allCharacters {
+			fmt.Println(c.Name)
+		}
+	} else if strings.ToLower(command) == "e" {
+		os.Exit(1)
+	}
 }
 
 func gatherCLICharacter() Character {
@@ -49,15 +85,41 @@ func gatherCLICharacter() Character {
 	c.Name = n
 	c.Id = uuid.New().String()
 	c.Health = h
+
+	var command string
+	fmt.Println("Would you like to fill out stats? Y or n?")
+	fmt.Scan(&command)
+	var s Stats
+	if strings.ToLower(command) == "y" {
+		fmt.Print("Strength:")
+		fmt.Scan(&s.Strength)
+
+		fmt.Print("Dexterity:")
+		fmt.Scan(&s.Dexterity)
+
+		fmt.Print("Constitution:")
+		fmt.Scan(&s.Constitution)
+
+		fmt.Print("Intelligence:")
+		fmt.Scan(&s.Intelligence)
+
+		fmt.Print("Wisdom:")
+		fmt.Scan(&s.Wisdom)
+
+		fmt.Print("Charisma:")
+		fmt.Scan(&s.Charisma)
+	}
+
 	return c
 }
 
-func (c *Character) writeCharacter(cDirectory string) {
+func (c *Character) writeCharacter(cDirectory string) error {
 	jsonData, err := json.Marshal(c)
 	if err != nil {
-		log.Fatal("Error marshaling json: ", err)
+		return err
 	}
 	os.WriteFile(fmt.Sprintf("%s%s.json", cDirectory, c.Id), jsonData, 0764)
+	return nil
 }
 
 func displayCharacter(c Character) {
